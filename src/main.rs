be28ab33ad73ce6e9af0d8363a6b2c06df70d43f
Tuomas Louekari <tuoppil@gmail.com>
2022-04-15 08:11:47 +0300

    Added blink led code

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,17 @@
+use sysfs_gpio;
+use sysfs_gpio::{Direction, Pin};
+use std::thread::sleep;
+use std::time::Duration;
+
 fn main() {
-    println!("Hello, world!");
+    let my_led = Pin::new(127); // number depends on chip, etc.
+    my_led.with_exported(|| {
+        my_led.set_direction(Direction::Out).unwrap();
+        loop {
+            my_led.set_value(0).unwrap();
+            sleep(Duration::from_millis(200));
+            my_led.set_value(1).unwrap();
+            sleep(Duration::from_millis(200));
+        }
+    }).unwrap();
 }
