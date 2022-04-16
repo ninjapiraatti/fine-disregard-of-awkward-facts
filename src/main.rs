900e33be7ad002364d471cf0ebf9808b55b8fe78
Tuomas Louekari <tuoppil@gmail.com>
2022-04-16 12:19:08 +0300

    Try to get a reading from ultra sonic sensor

--- src/main.rs
+++ src/main.rs
@@ -3,10 +3,14 @@ use rust_gpiozero::*;
 fn main() {
     // Create a new LED attached to Pin 18
     let mut led = LED::new(18);
+    let mut sonar1 = InputDevice::new(17);
+    let mut sonar2 = InputDevice::new(27);
 
     // on_time = 2 secs, off_time=3 secs
     led.blink(2.0,3.0);
 
     // prevent program from exiting immediately
+    println!("Sonar 1: {:?}", sonar1);
+    println!("Sonar 2: {:?}", sonar2);
     led.wait();
 }
