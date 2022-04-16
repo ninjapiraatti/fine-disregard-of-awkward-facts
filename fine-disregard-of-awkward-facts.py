c89336e4e4fa42c74dc0e04d50cfbfc1935a0812
Tuomas Louekari <tuoppil@gmail.com>
2022-04-16 12:46:13 +0300

    Try to get a reading from ultra sonic sensor

--- fine-disregard-of-awkward-facts.py
+++ fine-disregard-of-awkward-facts.py
@@ -1,10 +1,13 @@
 from gpiozero import LED
 from time import sleep
+from gpiozero import DistanceSensor
 
 led = LED(18)
+sensor = DistanceSensor(echo=22, trigger=27)
 
 while True:
     led.on()
     sleep(1)
     led.off()
+    print('Distance: ', sensor.distance * 100)
     sleep(1)
